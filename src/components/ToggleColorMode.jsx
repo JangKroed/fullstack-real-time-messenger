87ccc92323d644ff2e9b65c353565cdf22669b9a
JangKroed <didlsdydgh@gmail.com>
2023-11-24 00:06:51 +0900

    ^-^

---
+++ src/components/ToggleColorMode.jsx
@@ -0,0 +1,20 @@
+import { Button } from '@chakra-ui/react';
+import { useColorMode } from '@chakra-ui/color-mode';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+
+const ToggleColorMode = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+    return (
+        <Button
+            onClick={() => toggleColorMode()}
+            pos="absolute"
+            top="0"
+            right="0"
+            m="1rem"
+        >
+            {colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+        </Button>
+    );
+};
+
+export default ToggleColorMode;
