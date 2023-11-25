89db402d57225e48d337ec3d2599d407fa659b25
JangKroed <didlsdydgh@gmail.com>
2023-11-25 22:07:09 +0900

    common_error_fix_and_require_error_fix

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteCommonjs, esbuildCommonjs } from '@originjs/vite-plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [viteCommonjs(), react()],
+    optimizeDeps: {
+        plugins: [esbuildCommonjs(['@whatsapp.clone/common'])],
+    },
+});
